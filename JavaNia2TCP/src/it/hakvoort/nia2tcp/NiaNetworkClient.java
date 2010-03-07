96bfb3037d9b31765040f7c6e4b8f29bba26eda9
Gido Hakvoort <gido@hakvoort.it>
2010-03-07 12:54:06 +0000

    + import JavaNia2TCP project

---
+++ JavaNia2TCP/src/it/hakvoort/nia2tcp/NiaNetworkClient.java
@@ -0,0 +1,133 @@
+package it.hakvoort.nia2tcp;
+
+import it.hakvoort.nia.NiaListener;
+import it.hakvoort.nia.NiaSample;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class NiaNetworkClient {
+	
+	// the client socket
+	private Socket socket;
+	
+	// the input stream
+	private InputStream input;
+	
+	// the hostname or ip address of the server
+	private String HOST;
+	
+	// the server port
+	private int PORT;
+	
+	// if the client is connected
+	private boolean connected = false;
+
+	// the input reader
+	private NiaClientInputReader inputReader;
+	
+	// listeners waiting for records
+	protected List<NiaListener> listeners = new CopyOnWriteArrayList<NiaListener>();
+	
+	public NiaNetworkClient(String HOST, int PORT) {
+		this.HOST = HOST;
+		this.PORT = PORT;
+		
+		inputReader = new NiaClientInputReader();
+	}
+	
+	public void connect() {
+		try {
+			socket = new Socket();
+			socket.connect(new InetSocketAddress(HOST, PORT), 5000);
+			input = socket.getInputStream();
+
+			connected = true;
+		} catch (UnknownHostException e) {
+			System.err.println(String.format("Unknown Host: %s", HOST));
+		} catch (IOException e) {
+			System.err.println(String.format("Could not connect to %s:%s", HOST, PORT));
+		}
+		
+		if(connected) {
+			new Thread(inputReader).start();
+		}
+	}
+	
+	public void disconnect() {
+		if(!connected) {
+			return;
+		}
+
+		connected = false;
+
+		try {
+			input.close();
+			socket.close();
+		} catch (IOException e) {
+			System.err.println(String.format("Disconnect error"));
+		}
+	}
+	
+	public boolean isConnected() {
+		return this.connected;
+	}
+	
+	public void addListener(NiaListener listener) {
+		listeners.add(listener);
+	}
+	
+	public void removeListener(NiaListener listener) {
+		listeners.remove(listener);
+	}
+	
+	public void removeAllListeners() {
+		listeners.clear();
+	}
+	
+	public List<NiaListener> getListeners() {
+		return listeners;
+	}
+	
+	protected void fireReceivedSample(NiaSample sample) {
+		for(NiaListener listener : listeners) {
+			listener.receivedSample(sample);
+		}
+	}
+	
+	private class NiaClientInputReader implements Runnable {
+		
+		private int recordCounter = 0;
+		
+		public NiaClientInputReader() {
+			
+		}
+		
+		public void run() {
+			byte[] buffer = new byte[3];
+			
+			try {
+				while(connected && input.read(buffer) != -1) {
+					
+					int sample = (buffer[0] & 0xFF) | ((buffer[1] & 0xFF) << 8) | ((buffer[2] & 0xFF) << 16);
+					
+					if((sample & 0x800000) != 0) {
+						sample = ~(sample ^ 0x7fffff) + 0x800000;
+					}
+					
+					fireReceivedSample(new NiaSample(recordCounter, -1, -1, sample, -1));
+					recordCounter++;						
+				}
+			} catch(IOException e) {
+				
+			}
+			connected = false;
+			System.err.println("NiaNetworkClient disconnected.");
+		}
+	}
+}
