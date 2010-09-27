23b1246eb28f047ead6436e1e692bd196772538c
Gido Hakvoort <gido@hakvoort.it>
2010-09-27 10:56:48 +0000

    + lastest .bat scripts

--- JavaBDF/broadcast.bat
+++ JavaBDF/broadcast.bat
@@ -1,4 +1,5 @@
 @echo off
+
 IF %1.==. (SET host=127.0.0.1) 	ELSE (SET host=%1)
 IF %2.==. (SET hostport=778) 	ELSE (SET hostport=%2)
 IF %3.==. (SET channels=32) 	ELSE (SET channels=%3)
--- JavaBDF/server.bat
+++ JavaBDF/server.bat
@@ -1,5 +1,6 @@
 @echo off
+
 IF %1.==. (SET arg1=./data/bdf/example.bdf) 	ELSE (SET arg1=%1)
-IF %2.==. (SET arg2=778) 						ELSE (SET arg2=%2)
+IF %2.==. (SET arg2=778) 			ELSE (SET arg2=%2)
 
 java -cp ./lib/JavaBDF.jar it.hakvoort.bdf.network.BDFServer %arg1% %arg2% %3 %4
